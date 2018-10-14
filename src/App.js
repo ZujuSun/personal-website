2059441285488360d6f818a1d0750a3f07101ba1
Zuju Sun <sunzuju@gmail.com>
2018-10-13 23:23:26 -0400

    add linkage for pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import NavigationBar from "./NavigationBar.js";
+import NavigationBar from "./NavigationBar";
+import Home from './Home';
+import Experience from './Experience';
+import NotFound from './NotFound'
+import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
-      <NavigationBar/>
+      <Router>
+        <div>
+        <NavigationBar/>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/experience" component={Experience} />
+            <Route path="/404" component={NotFound} />
+            <Redirect from="*" to="/404" />
+        </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Experience extends React.Component {
+    render() {
+      return (
+        <h1>this is Experience page</h1>
+      );
+    }
+
+}
+
+export default Experience;
